d85b5859adb1fccde0116f8c712ae19a2426c1fa
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-09-12 19:15:40 +0200

    Write un-implemented undo append scenario

--- src/test/java/com/mathieupauly/undokatajava/feature/glue/UndoSteps.java
+++ src/test/java/com/mathieupauly/undokatajava/feature/glue/UndoSteps.java
@@ -1,9 +1,32 @@
 package com.mathieupauly.undokatajava.feature.glue;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class UndoSteps {
-    @When("^undo$")
-    public void undo() {
+    @Given("^append has been performed$")
+    public void append_has_been_performed() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^the buffer printed \"([^\"]*)\"$")
+    public void the_buffer_printed(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^writer performs undo command$")
+    public void writer_performs_undo_command() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the buffer should print \"([^\"]*)\"$")
+    public void the_buffer_should_print(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 }
--- src/test/resources/com/mathieupauly/undokatajava/feature/undo.feature
+++ src/test/resources/com/mathieupauly/undokatajava/feature/undo.feature
@@ -1,3 +1,6 @@
 Feature: Undo command
   Scenario: Append
-    When undo
+    Given append has been performed
+    And the buffer printed "Hello"
+    When writer performs undo command
+    Then the buffer should print "Hell"
