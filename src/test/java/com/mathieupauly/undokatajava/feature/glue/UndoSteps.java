bcb42185e002b84ea58907083816a82d798e1598
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-09-12 19:20:45 +0200

    Bind (still failing) undo append scenario with a glue code

--- src/test/java/com/mathieupauly/undokatajava/feature/glue/UndoSteps.java
+++ src/test/java/com/mathieupauly/undokatajava/feature/glue/UndoSteps.java
@@ -1,32 +1,34 @@
 package com.mathieupauly.undokatajava.feature.glue;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UndoSteps {
+    private String buffer;
+
     @Given("^append has been performed$")
-    public void append_has_been_performed() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void append_has_been_performed() {
     }
 
     @Given("^the buffer printed \"([^\"]*)\"$")
-    public void the_buffer_printed(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void the_buffer_printed(String buffer) {
+        this.buffer = buffer;
     }
 
     @When("^writer performs undo command$")
-    public void writer_performs_undo_command() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void writer_performs_undo_command() {
+        buffer = undo();
     }
 
     @Then("^the buffer should print \"([^\"]*)\"$")
-    public void the_buffer_should_print(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void the_buffer_should_print(String expectedBuffer) {
+        assertThat(buffer).isEqualTo(expectedBuffer);
+    }
+
+    private String undo() {
+        return null;
     }
 }
