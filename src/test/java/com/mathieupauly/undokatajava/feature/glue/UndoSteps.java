1090f03fcc2d2a8f551807408b1af5a4d87c31bd
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-09-12 19:12:47 +0200

    Make the setup scenario fail to test cucumber runner

---
+++ src/test/java/com/mathieupauly/undokatajava/feature/glue/UndoSteps.java
@@ -0,0 +1,12 @@
+package com.mathieupauly.undokatajava.feature.glue;
+
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.fail;
+
+public class UndoSteps {
+    @When("^undo$")
+    public void undo() {
+        fail();
+    }
+}
