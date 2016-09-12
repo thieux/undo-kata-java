323e6382705ad8d6dd91599f7580bafafe1915c3
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-09-12 19:06:56 +0200

    Setup cucumber infrastructure

---
+++ src/test/java/com/mathieupauly/undokatajava/feature/FeatureRunnerTest.java
@@ -0,0 +1,8 @@
+package com.mathieupauly.undokatajava.feature;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class FeatureRunnerTest {
+}
---
+++ src/test/resources/com/mathieupauly/undokatajava/feature/undo.feature
@@ -0,0 +1,3 @@
+Feature: Undo command
+  Scenario: Append
+    When undo
